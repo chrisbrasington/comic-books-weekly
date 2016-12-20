6ca03ad2d55ab5e4af17b82bbf05d01c15b71287
chrisbrasington <chrisbrasington@gmail.com>
2016-12-19 19:09:02 -0700

    add last week and next week to parsing

--- comics.rb
+++ comics.rb
@@ -1,28 +1,24 @@
 #!/usr/bin/env ruby
 require 'rss'
 require 'open-uri'
+require 'nokogiri'
 require 'yaml'
 require 'net/https'
 require './settings'
 
 # remove html
 def sanitize s
-  s = s.to_s.gsub( %r{</?[^>]+?>}, '' )
-  s = s.sub! '/&gt;', ''
-  s = s.sub! '&lt;', ''
-  s = s.sub! " \n", ''
-  s = s.gsub /&amp;amp;/, '&'
+   s = Nokogiri::HTML(s).text
+   s = s.gsub(/\(.*\)/, "")
 end
 
 # comic class from csv
 class Comic
-  attr_accessor :date, :publisher, :name, :price
+  attr_accessor :name, :price
   def initialize(csv)
     data = sanitize(csv).split(',')
-    @date = data[0]
-    @publisher = data[1]
-    @name = data[2]
-    @price = data[3]
+    @name = data[0].chomp(' ')
+    @price = data[1].delete(' ')
   end
   def to_s
     s = @name
@@ -31,6 +27,7 @@ end
 
 # pushover notification
 def pushover(apptoken, usertoken, message)
+  puts
   puts 'Responding via pushover.'
   url = URI.parse("https://api.pushover.net/1/messages.json")
   req = Net::HTTP::Post.new(url.path)
@@ -66,64 +63,132 @@ def get_wednesday
   return Date.today - (Date.today.wday - 3)
 end
 
+# parse a week of comic content
+def parse_feed_item(item, pull)
+  # found comics
+  comics = []
+
+  # clean up item
+  item = Nokogiri::HTML(item.to_s).text.to_s
+  item = item.gsub("\n",'')
+
+  # split per row
+  item.to_s.split('<br />').each do |row|
+
+    # looking only for numbered issues (not tradeback (TP) or AR merchandise)
+    # remove the + ' #' if you want to allow less 'full titles'
+    #   for example, 'Star Wars' will pick up seoncary comics like 'Star Wars: Han Solo'
+    #   with the space #, it'll only pick up strictly 'Star Wars #' comics of that single series
+    if row.include? '#' and row.include? '$' and pull.any?{ |c| row.to_s.downcase.include? c + ' #' }
+    
+      # add comic
+      comic = Comic.new(row.to_s)
+      comics.push(comic)
+
+      # remove from pull (prevents dupes, usually from variants)
+      pull.delete_if{|c| comic.name.downcase.include? c.downcase}
+    end
+  end
+  # sort and print
+  comics = comics.sort_by { |c| [-c.name] }
+  
+  return comics
+end
+
 # weekly comic feed
-url = 'http://feeds.feedburner.com/ncrl?format=xml'
+url_this_week = 'http://feeds.feedburner.com/comiclistfeed?format=xml'
+
+# next weeks comic feed
+url_next_week = 'http://feeds.feedburner.com/comiclistnextweek?format=xml'
 
 # pull list of comics
-pull = File.read('pull.txt').split("\n").map(&:downcase)
-
-# found comics
-comics = []
+pull = File.read('/home/christopher/repo/comic-books-weekly/pull.txt').split("\n").map(&:downcase)
 
 # comics released every Wednesday
-wednesday = get_wednesday().strftime("%m/%d/%Y")
+wednesday = get_wednesday()
 
-open(url) do |rss|
+# first check this week feed (includes last week)
+# next check next weeks feed
+feed = ''
+
+# this week
+open(url_this_week) do |rss|
   # parse RSS
   feed = RSS::Parser.parse(rss)
+end
 
-  # first item is this week
-  feed.items.each do |item|
+# pushover message
+full_message = ''
 
-    # split per row
-    item.to_s.split('br').each do |row|
+# parse this week, then last week
+iterations = 0
+feed.items.each do |item|
+  
+  comics = parse_feed_item(item, pull)
 
-      # looking only for numbered issues (not tradeback (TP) or AR merchandise)
-      # remove the + ' #' if you want to allow less 'full titles'
-      #   for example, 'Star Wars' will pick up seoncary comics like 'Star Wars: Han Solo'
-      #   with the space #, it'll only pick up strictly 'Star Wars #' comics of that single series
-      if row.include? '#' and row.include? '$' and pull.any?{ |c| row.downcase.include? c + ' #' }
+  # wednesday
+  message = "(#{wednesday.strftime("%m/%d/%Y")})\n"
 
-        # add comic
-        comic = Comic.new(row)
-        comics.push(comic)
-
-        # remove from pull (prevents dupes, usually from variants)
-        pull.delete_if{|c| comic.name.downcase.include? c.downcase}
-      end
-    end
-    break;
+  # this week or next week  
+  if iterations == 0
+    message = 'This Week ' + message
+  elsif iterations == 1
+    message = 'Last Week ' + message
   end
+  
+  # add each comic to message
+  comics.each do |c|
+    message += c.to_s
+    message += "\n"
+  end
+
+  # display
+  puts message
+  puts
+  
+  # go back a week (if continuing in feed
+  wednesday = wednesday - 7
+  iterations = iterations +1
+
+  # append message  
+  full_message += message
+  full_message += "\n"
 end
 
-# sort and print
-comics = comics.sort_by { |c| [-c.name] }
-
-# create message
-message = "Comics released on #{wednesday}\n"
-comics.each do |c|
-  message += c.to_s
-  message += "\n"
+# next week feed
+open(url_next_week) do |rss|
+  # parse RSS
+  feed = RSS::Parser.parse(rss)
 end
 
-# display message
-message = message.chomp("\n")
-puts message
+# parse next week
+feed.items.each do |item|
+  comics = parse_feed_item(item, pull)
+  
+  message = 'Next Week '
+  message = message + "(#{(get_wednesday()+7).strftime("%m/%d/%Y")})\n"
+    
+  # add each comic to message
+  comics.each do |c|
+    message += c.to_s
+    message += "\n"
+  end
+  
+  # display
+  puts message
+  
+  # append message
+  full_message += message
+  
+  # break we've already checked prior weeks
+  break;
+
+end
 
 # if pushover setting exists, respond via pushover
 settings = Settings.new
 if !settings.list['pushover'].nil?
   apitoken = settings.list['pushover']['apitoken']
   usertoken = settings.list['pushover']['userkey']
-  pushover(apitoken, usertoken, message)
+  pushover(apitoken, usertoken, full_message)
 end
